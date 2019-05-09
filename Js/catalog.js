afaf814bfae9f74409e68e93131a97eeb8b9d8b1
laoli <laoli@qq.com>
2019-05-09 21:02:52 +0800

    第二天

---
+++ Js/catalog.js
@@ -0,0 +1,69 @@
+
+
+$("#ys1").on('mouseover',function(){
+    $("#ys1-sj").css('display','block');
+    $('#nav').css('background-color','#fff');
+    $(".nav-link  a,.logo-shop,.logo-logon").css('color','#333');
+    $("#ys1 a").css('color','#2e74f6');
+});
+$("#ys1").on('mouseout',function(){
+    $('#ys1-sj').css('display','none');
+    $('#nav').css('background-color','');
+    $(".nav-link  a,.logo-shop,.logo-logon").css('color','');
+});
+
+$("#ys2").on('mouseover',function(){
+    $("#ys2-sx").css('display','block');
+    $('#nav').css('background-color','#fff');
+    $(".nav-link  a,.logo-shop,.logo-logon").css('color','#333');
+    $("#ys2 a").css('color','#2e74f6');
+});
+$("#ys2").on('mouseout',function(){
+    $('#ys2-sx').css('display','none');
+    $('#nav').css('background-color','');
+    $(".nav-link  a,.logo-shop,.logo-logon").css('color','');
+});
+
+
+
+
+$("#ys3").on('mouseover',function(){
+    $("#ys1-sj").css('display','block');
+    $('#nav').css('background-color','#fff');
+    $(".nav-link  a,.logo-shop,.logo-logon").css('color','#333');
+    $("#ys3 a").css('color','#2e74f6');
+});
+$("#ys3").on('mouseout',function(){
+    $('#ys1-sj').css('display','none');
+    $('#nav').css('background-color','');
+    $(".nav-link  a,.logo-shop,.logo-logon").css('color','');
+});
+
+
+$("#ys4").on('mouseover',function(){
+    $("#ys2-sx").css('display','block');
+    $('#nav').css('background-color','#fff');
+    $(".nav-link  a,.logo-shop,.logo-logon").css('color','#333');
+    $("#ys4 a").css('color','#2e74f6');
+});
+$("#ys4").on('mouseout',function(){
+    $('#ys2-sx').css('display','none');
+    $('#nav').css('background-color','');
+    $(".nav-link  a,.logo-shop,.logo-logon").css('color','');
+});
+
+
+
+
+$("#ys9").on('mouseover',function(){
+    $("#ys9-xz").css('display','block');
+    $('#nav').css('background-color','#fff');
+    $(".nav-link  a,.logo-shop,.logo-logon").css('color','#333');
+    $("#ys9 a").css('color','#2e74f6');
+});
+$("#ys9").on('mouseout',function(){
+    $('#ys9-xz').css('display','none');
+    $('#nav').css('background-color','');
+    $(".nav-link  a,.logo-shop,.logo-logon").css('color','');
+});
+
